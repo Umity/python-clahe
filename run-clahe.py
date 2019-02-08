16989b6409f36e9381c044d862b9de9e0e85c822
Umity <kusadasiguy@gmail.com>
2019-02-08 22:20:16 +0300

    Create run-clahe.py

---
+++ run-clahe.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+bgr = cv2.imread('Desktop/img-import.jpg')
+lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+lab_planes = cv2.split(lab)
+clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+lab_planes[0] = clahe.apply(lab_planes[0])
+lab = cv2.merge(lab_planes)
+bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+cv2.imwrite('Desktop/img-output.jpg',bgr)
